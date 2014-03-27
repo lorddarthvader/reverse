72983cd945ef481607096601efc94700ff864f88
lorddarthvader <ilubmonsters@gmail.com>
2014-03-27 16:47:05 +0530

    Create reverse.cpp

---
+++ reverse.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+
+/***
+ *ynnoD yb dedoC
+ *)stsixe elif eht fi( elif a fo stnetnoc eht ro txet nevig eht desreveR
+ *tixe ot D+lrtC
+ ***/
+
+std::string reverse(std::string txt) 
+{
+  std::string reversed = "";
+  for(int i = txt.length(); i >= 0; i--)
+    reversed += txt[i];
+  return reversed;
+}
+
+int main() 
+{
+  std::string str;
+  while(std::getline(std::cin, str)) 
+    {
+      std::ifstream file_(str.c_str());
+      if(file_.is_open()) 
+	{
+	  std::string token;
+	  while(std::getline(file_, token))
+	    std::cout << reverse(token) << std::endl;
+	  file_.close();
+	}else 
+	{
+	  std::cout << reverse(str) << std::endl;
+	}
+    }
+}
